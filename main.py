9880120a903a22c316b7c319c1406bac3aa60bdd
ednelson89 <ednelson89@gmail.com>
2024-06-20 05:12:09 -0400

    added test mode and some validation to add_char

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ class Character:
 
 
 initiative_tracker = []
+test_mode = False
 
-initiative_tracker.append(Character("test1", 20, 0))
-initiative_tracker.append(Character("test2", 18, 1))
-initiative_tracker.append(Character("test3", 18, 2))
+# For test/demo purposes, allow a command line arg to populate the order
+if len(sys.argv) == 2 and sys.argv[1] == "test":
+    test_mode = True
+    initiative_tracker.append(Character("test1", 20, 0))
+    initiative_tracker.append(Character("test2", 18, 1))
+    initiative_tracker.append(Character("test3", 18, 2))
+    initiative_tracker.append(Character("test4", 5, 1))
+    initiative_tracker.append(Character("test5", 5, 2))
+    initiative_tracker.append(Character("test5", 10, 0))
 
 
 def main():
+    clear()
     print("***Welcome to the Initiaive Tracker***")
 
     run = True
     while run == True:
+        if test_mode == True:
+            print("### Test Mode ###")
+
         command = instruction_query()
 
         match command:
@@ -31,6 +42,9 @@ def main():
                 input()
             case "3":  # 3 Change a characters initiative
                 update_init()
+                input()
+            case "4":  # 4 Remove a character from the initiative order
+                del_char()
                 input()
             case "0":
                 print("Thank you for using the Initiative Tracker")
@@ -59,6 +73,7 @@ def instruction_query():
     print("1. Display Current Order")
     print("2. Add a Character")
     print("3. Change a character's initiative")
+    print("4. Remove a character from the initiative order")
     print("0. EXIT")
 
     print("Please enter the number of the option you'd like:")
@@ -67,15 +82,25 @@ def instruction_query():
 
 
 def add_char():
-    print("Please enter a character name:")
+    print("Please enter a character name (required):")
     tempChar = input()
+    if not tempChar:
+        print("Character name required. Please try again.")
+        return
+
     print("Please enter initiative:")
     tempInit = input()
+    if not tempInit:
+        tempInit = 0
+
     print("Please enter dex mod:")
     tempDex = input()
+    if not tempDex:
+        tempDex = 0
 
     initiative_tracker.append(Character(tempChar, int(tempInit), int(tempDex)))
     print(f"You've added {tempChar} to the initiative order.")
+    update_init_msg(True)
 
 
 def update_init():
@@ -91,10 +116,30 @@ def update_init():
             entry.initiative = int(input())
             print("Enter the new dex mod value:")
             entry.dexMod = int(input())
+
+    update_init_msg(match)
+
+
+def del_char():
+    print("Please enter a character name:")
+    tempChar = input()
+    match = False
+    for ind, entry in enumerate(initiative_tracker):
+        if entry.name == tempChar:
+            match = True
+            print(f"Are you sure you want to delete: {entry.name}? (y/n, yes/no)")
+            ans = input()
+            if ans == "y" or ans == "yes":
+                del initiative_tracker[ind]
+
+    update_init_msg(match)
+
+
+def update_init_msg(match):
     if match == False:
         print("Failed to find a character with that name; check your spelling.")
     else:
-        print("Updated initiative.")
+        print("Updated the initiative order.")
 
 
 def clear():
